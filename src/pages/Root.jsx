85b7497e6530de4f4ee9b07a3d2287f7c1ac1c42
Mukesh <billamukesh1234@gmail.com>
2024-01-07 11:42:06 +0530

    added pre-loader

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,29 +1,75 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../layouts/Navbar";
-// import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import mlscLogo from '../assets/mlsclogo.png';
 
 const Root = () => {
-    // const [color, setColor] = useState('white');
-    // const { pathname } = useLocation()
-    // // console.log(pathname);
 
-    // useEffect(() => {
-    //     if (pathname === '/about') {
-    //         setColor('black');
-    //     }
-    //     else {
-    //         setColor('white');
-    //     }
-    // }, [pathname])
+    useEffect(() => {
+        const preloader = document.getElementById('preloader');
+        const ctnPreloader = document.getElementById('ctn-preloader');
+
+        setTimeout(() => {
+            ctnPreloader.classList.add('loaded');
+            document.body.classList.remove('no-scroll-y');
+
+            if (ctnPreloader.classList.contains('loaded')) {
+                setTimeout(() => {
+                    preloader.parentNode.removeChild(preloader);
+                }, 1000);
+            }
+        }, 3000);
+    }, []);
+
 
     return (
         <>
-            {/* <Navbar aboutClr={color} /> */}
             <Navbar />
             <Outlet />
-            {/* <div className="preloader">
-                MLSC
-            </div> */}
+            <section>
+                <div id="preloader">
+                    <div id="ctn-preloader" className="ctn-preloader">
+                        <div className="animation-preloader">
+                            <img src={mlscLogo} alt="mlsc logo" className="fadeInOut"/>
+                            <div className="txt-loading">
+                                <span data-text-preloader="M" className="letters-loading">
+                                    M
+                                </span>
+
+                                <span data-text-preloader="L" className="letters-loading">
+                                    L
+                                </span>
+
+                                <span data-text-preloader="S" className="letters-loading">
+                                    S
+                                </span>
+
+                                <span data-text-preloader="C" className="letters-loading">
+                                    C
+                                </span>
+
+                                <span data-text-preloader="V" className="letters-loading">
+                                    V
+                                </span>
+
+                                <span data-text-preloader="C" className="letters-loading">
+                                    C
+                                </span>
+
+                                <span data-text-preloader="E" className="letters-loading">
+                                    E
+                                </span>
+
+                                <span data-text-preloader="T" className="letters-loading">
+                                    T
+                                </span>
+                            </div>
+                        </div>
+                        <div className="loader-section section-left"></div>
+                        <div className="loader-section section-right"></div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
