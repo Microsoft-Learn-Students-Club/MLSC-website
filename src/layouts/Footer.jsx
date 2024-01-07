a8afff654c163f7dc9bf8e6611b96b83d5ce8013
Mukesh <billamukesh1234@gmail.com>
2024-01-07 15:07:19 +0530

    added pre-loader

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,5 +1,6 @@
 import mlscLogo from '../assets/mlsclogo.png'
 import styles from './Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     return (
@@ -12,15 +13,18 @@ const Footer = () => {
                         </div>
                         <div className={styles.footer_contact}>
                             <h4>Contact us</h4>
-                            <p>Rishabh Tripathi: +91 9369744734</p>
-                            <p>Kshitij Patil: +91 9323544113</p>
+                            <div className={`flex_center | ${styles.contacts}`}>
+                                {/* <p><span><FontAwesomeIcon icon="fa-solid fa-envelope" /></span> mlsc@vcet.edu</p> */}
+                                <p>Rishabh Tripathi: +91 9369744734</p>
+                                <p>Kshitij Patil: +91 9323544113</p>
+                            </div>
                         </div>
                         <div className={styles.footer_social}>
                             <h4>Follow us</h4>
                             <div className={`flex_center | ${styles.socialmedia}`}>
-                                <a className={styles.social_icon} href="https://www.linkedin.com/company/mlsc-vcet/mycompany/" target="_blank" rel='noreferrer'><i className="fab fa-linkedin"></i></a>
-                                <a className={styles.social_icon} href="https://github.com/Microsoft-Learn-Students-Club" target="_blank" rel='noreferrer'><i className="fab fa-github"></i></a>
-                                <a className={styles.social_icon} href="https://www.instagram.com/mlsc_vcet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D" target="_blank" rel='noreferrer'><i className="fab fa-instagram"></i></a>
+                                <a className={styles.social_icon} href="https://www.linkedin.com/company/mlsc-vcet/mycompany/" target="_blank" rel='noreferrer'><FontAwesomeIcon icon="fa-brands fa-linkedin" /></a>
+                                <a className={styles.social_icon} href="https://github.com/Microsoft-Learn-Students-Club" target="_blank" rel='noreferrer'><FontAwesomeIcon icon="fa-brands fa-github" /></a>
+                                <a className={styles.social_icon} href="https://www.instagram.com/mlsc_vcet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D" target="_blank" rel='noreferrer'><FontAwesomeIcon icon="fa-brands fa-instagram" /></a>
                             </div>
                         </div>
                     </div>
