016951773fcfcfdffad63a67da4392d6a87aac11
Adarsh Gupta <csedsadarsh@gmail.com>
2024-01-14 17:58:06 +0530

    resolve fliker issue and added preloader

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,36 +1,11 @@
-import { useEffect } from 'react';
-import styles from './Loading.module.css';
-
+import styles from "./Loading.module.css";
+import bgvideo from "../assets/mlsc.mp4";
 
 const Loading = () => {
-
-    useEffect(() => {
-        const loading = document.getElementById('loading');
-        const loader = document.getElementById('loader');
-
-        setTimeout(() => {
-            loader.classList.add('loaded');
-
-            if (loader.classList.contains('loaded')) {
-                setTimeout(() => {
-                    loading.parentNode.removeChild(loading);
-                }, 1000);
-            }
-        }, 3000);
-    }, []);
-
-    return (
-        <>
-            <section className={styles.loading}>
-                <div id="loader" className={`${styles.loader}`}>
-                    <div className={styles.animation_loader}>
-                        <div className={styles.spinner}></div>
-                    </div>
-                    <div className={`${styles.loader_section} ${styles.section_left}`}></div>
-                    <div className={`${styles.loader_section} ${styles.section_right}`}></div>
-                </div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <video src={bgvideo} loop autoPlay muted></video>
+    </>
+  );
+};
 export default Loading;
