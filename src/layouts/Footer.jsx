798255a467885c98105dbaddcbec92792dfe21ee
Mukesh <billamukesh1234@gmail.com>
2024-01-15 22:16:03 +0530

    updated footer part

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => {
                         <div className={styles.footer_social}>
                             <div className={`flex_center | ${styles.social_media}`}>
                                 <ul className={styles.social_media_content}>
-                                    <li className={styles.flex_li}><a href=''><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Twitter</h3></a></li>
-                                    <li className={styles.flex_li}><a href=''><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Github</h3></a></li>
+                                    <li className={styles.flex_li}><a href='https://twitter.com/'><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Twitter</h3></a></li>
+                                    <li className={styles.flex_li}><a href='https://github.com/Microsoft-Learn-Students-Club'><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Github</h3></a></li>
                                     <li className={styles.flex_li}><a href='https://codingadda.hashnode.dev/'><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Hashnode</h3></a></li>
                                 </ul>
                             </div>
@@ -23,9 +23,9 @@ const Footer = () => {
                         <div className={styles.footer_social}>
                             <div className={`flex_center | ${styles.social_media}`}>
                                 <ul className={`${styles.social_media_content}`}>
-                                    <li className={styles.flex_li}><a href=''><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Instagram</h3></a></li>
-                                    <li className={styles.flex_li}><a href=''><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Youtube</h3></a></li>
-                                    <li className={styles.flex_li}><a href=''><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Linkedin</h3></a></li>
+                                    <li className={styles.flex_li}><a href='https://www.instagram.com/mlsc_vcet/'><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Instagram</h3></a></li>
+                                    <li className={styles.flex_li}><a href='https://www.youtube.com/@coding_adda'><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Youtube</h3></a></li>
+                                    <li className={styles.flex_li}><a href='https://www.linkedin.com/company/mlsc-vcet/'><h3><span><FontAwesomeIcon icon="fa-solid fa-square-full" /></span>Linkedin</h3></a></li>
                                 </ul>
                             </div>
                         </div>
