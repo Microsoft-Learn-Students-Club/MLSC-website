3bec6ef3c098be7456fbd941744339ecd949eb43
Mukesh <billamukesh1234@gmail.com>
2024-03-05 01:05:09 +0530

    change: fully responsive

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -16,6 +16,30 @@ const Events = ({count}) => {
 
   const eventsData = [
     {
+      title: "SQL Workshop by Kantascrypt ",
+      Speaker: "Mr. Samtani Edwin",
+      imgSrc: SqlWorkshop,
+      description: `Microsoft Learn Students Club hosted a SQL workshop featuring guest speaker Mr. Samtani Edwin, Lead Database Architect of Kantascrypt, offering hands-on training in SQL fundamentals. Participants gained practical skills in database management, query optimization, and data manipulation techniques, culminating in a certificate of completion.`,
+      info: [
+        { label: "Organizing People", value: 8 },
+        { label: "Participants", value: 175 },
+        { label: "Date and Time", value: `22nd Feb, 2 PM` },
+        { label: "Event Span", value: `1 day`  },
+      ],
+    },
+    {
+      title: "Guest Lecture by Scogo Networks",
+      Speaker: "Mr. Karan Singh",
+      imgSrc: scogoTuring,
+      description: ` Microsoft Learn Students Club organized a seminar titled "From Turing to Transformers: AI's Revolutionary Tale" featuring guest speaker Mr. Karan Singh, Co-founder and CTO of Scogo Networks, elucidating AI's transformative journey. Attendees delved into AI's impact across diverse sectors, gaining valuable insights, and received certificates recognizing their participation and engagement in the event.`,
+      info: [
+        { label: "Organizing People", value: 8 },
+        { label: "Participants", value: 171 },
+        { label: "Date and Time", value: `20th Feb, 11:30 AM` },
+        { label: "Event Span", value: `1 day`  },
+      ],
+    },
+    {
       title: "ReactJS Workshop",
       imgSrc: workshopEventImg,
       description: `Participants in MLSC's recent React JS workshop engaged in hands-on coding sessions under 
@@ -42,30 +66,6 @@ const Events = ({count}) => {
         { label: "Participants", value: 221 },
         { label: "Date and Time", value: `26th Sept to 17th Oct 2023, 9 AM` },
         { label: "Event Span", value: `22 days`  },
-      ],
-    },
-    {
-      title: "Guest Lecture by Scogo Networks",
-      Speaker: "Mr. Karan Singh",
-      imgSrc: scogoTuring,
-      description: ` Microsoft Learn Students Club organized a seminar titled "From Turing to Transformers: AI's Revolutionary Tale" featuring guest speaker Mr. Karan Singh, Co-founder and CTO of Scogo Networks, elucidating AI's transformative journey. Attendees delved into AI's impact across diverse sectors, gaining valuable insights, and received certificates recognizing their participation and engagement in the event.`,
-      info: [
-        { label: "Organizing People", value: 8 },
-        { label: "Participants", value: 171 },
-        { label: "Date and Time", value: `20th Feb, 11:30 AM` },
-        { label: "Event Span", value: `1 day`  },
-      ],
-    },
-    {
-      title: "SQL Workshop by Kantascrypt ",
-      Speaker: "Mr. Samtani Edwin",
-      imgSrc: SqlWorkshop,
-      description: `Microsoft Learn Students Club hosted a SQL workshop featuring guest speaker Mr. Samtani Edwin, Lead Database Architect of Kantascrypt, offering hands-on training in SQL fundamentals. Participants gained practical skills in database management, query optimization, and data manipulation techniques, culminating in a certificate of completion.`,
-      info: [
-        { label: "Organizing People", value: 8 },
-        { label: "Participants", value: 175 },
-        { label: "Date and Time", value: `22nd Feb, 2 PM` },
-        { label: "Event Span", value: `1 day`  },
       ],
     },
   ];
